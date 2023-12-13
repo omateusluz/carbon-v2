f89a88cbc8110900bb6d627399212954eb170be7
Matheus Luz <78989307+omateusluz@users.noreply.github.com>
2023-12-13 16:29:33 -0300

    cadastro e login funcionando

---
+++ front/src/main.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+//Configuracoes do React Router (npm install react-router-dom)
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+//Importando rotas
+
+import Cadastro from './paginas/cadastro/Cadastro.jsx';
+import Configuracoes from './paginas/configuracoes/Configuracoes.jsx';
+import Dashboard from './paginas/dashboard/Dashboard.jsx';
+import Detalhes from './paginas/detalhes/Detalhes.jsx';
+import Entrar from './paginas/entrar/Entrar.jsx';
+import EnvioUm from './paginas/envio-um/EnvioUm.jsx';
+import EnvioDois from './paginas/envio-dois/EnvioDois.jsx';
+import EnvioTres from './paginas/envio-tres/EnvioTres.jsx';
+import Home from './paginas/home/Home.jsx';
+import NovoEmailUm from './paginas/novo-email-um/NovoEmailUm.jsx';
+import NovoEmailDois from './paginas/novo-email-dois/NovoEmailDois.jsx';
+import RecuperarUm from './paginas/recuperar-um/RecuperarUm.jsx';
+import RecuperarDois from './paginas/recuperar-dois/RecuperarDois.jsx';
+import RecuperarTres from './paginas/recuperar-tres/RecuperarTres.jsx';
+import NaoEncontrado from './paginas/NaoEncontrado.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  },
+  {
+    path: 'cadastro',
+    element: <Cadastro />
+  },
+  {
+    path: 'configuracoes',
+    element: <Configuracoes />
+  },
+  {
+    path: 'dashboard',
+    element: <Dashboard />
+  },
+  {
+    path: 'detalhes',
+    element: <Detalhes />
+  },
+  {
+    path: 'entrar',
+    element: <Entrar />
+  },
+  {
+    path: 'envio-um',
+    element: <EnvioUm />
+  },
+  {
+    path: 'envio-dois',
+    element: <EnvioDois />
+  },
+  {
+    path: 'envio-tres',
+    element: <EnvioTres />
+  },
+  {
+    path: 'email-um',
+    element: <NovoEmailUm />
+  },
+  {
+    path: 'email-dois',
+    element: <NovoEmailDois />
+  },
+  {
+    path: 'recuperar-um',
+    element: <RecuperarUm />
+  },
+  {
+    path: 'recuperar-dois',
+    element: <RecuperarDois />
+  },
+  {
+    path: 'recuperar-tres',
+    element: <RecuperarTres />
+  },
+  {
+    path: 'nao-encontrado',
+    element: <NaoEncontrado />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
---
+++ front/src/paginas/cadastro/Cadastro.jsx
@@ -0,0 +1,86 @@
+import '../../styles/Cadastrar.css';
+import {useForm} from 'react-hook-form';
+import axios from 'axios';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
+export default function Cadastro(){
+    
+    const [msg, setMsg] = useState();
+    const [setUserCriado] = useState(false);
+
+    const schema = yup.object({
+        username: yup.string().required('Usuário obrigatório'),
+        email: yup.string().email('Email inválido').required('Email obrigatório'),
+        password: yup.string().min(4,'Senha com no mínimo 4 caracteres').required(),
+        passwordConf: yup.string().required('Confirme a senha').oneOf([yup.ref('password')], 'As senhas devem coincidir!'),
+    });
+
+    const form = useForm({
+        resolver: yupResolver(schema)
+    });
+
+
+    const { register, handleSubmit, formState } = form;
+
+    const {errors} = formState;
+
+    const submit = async (data) => {
+        
+        try {
+            const response = await axios.post('http://localhost:3000/create', data);
+            setMsg(response.data);
+            if(response.data.includes('sucesso'))
+                setUserCriado(true);
+        } catch (error) {
+            setMsg(error.response.data);
+        }   
+    
+    }
+
+    return (
+        <div className="geral">
+          <div className="div">
+            <div className="h-wrapper">
+              <div className="text-wrapper">Crie uma nova conta</div>
+            </div>
+            <div className="frame">
+              <p className="h">Insira os dados para cadastrar.</p>
+            </div>
+            <img className="line" alt="Line"/>
+
+            <form onSubmit={handleSubmit(submit)} noValidate>
+                <div className="h-6">Nome *</div>
+                <input className="input-A" type="text" id="username" placeholder="nome de usuario" {...register('username')} />
+
+                <div className="h-5">Email *</div>
+                <input className="input-B" placeholder="endereco de email" type="text" id="email" {...register('email')} />
+
+                <div className="h-4">Senha *</div>
+                <input className="input-C" placeholder="senha" type="password" id="password" {...register('password')} />
+
+                <div className="h-3">Confirmação da senha *</div>
+                <input className="input-D" placeholder="confirmacao da senha" type="password" id="passwordConf" {...register('passwordConf')} />
+
+                <div className="p-2">Mínimo de 4 caracteres</div>
+                <button className="button">
+                    <div className="h-2">Cadastrar -&gt;</div>
+                </button>
+
+            </form>
+            
+            <div className="p-wrapper">
+              <a className="p" href="/entrar">{`Tenho uma conta ->`}</a>
+            </div>
+            
+          </div>
+          <p className='erro'>{errors.username?.message}</p>
+             <p className='erro'>{errors.email?.message}</p>
+             <p className='erro'>{errors.password?.message}</p>
+             <p className='erro'>{errors.passwordConf?.message}</p>
+             <p className='server-response'>{msg}</p>   
+        </div>
+      );
+    };
